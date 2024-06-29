0d067e465c9074aba7084ccbc65eb8108989ae2f
Ethan Zhang <ethanzhangyixuan@gmail.com>
2024-06-29 16:31:35 +1000

    first few code

--- python/practicepython.py
+++ python/practicepython.py
@@ -1,1 +1,12 @@
 # HELLO
+
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+if mystring == "hello":
+    print("String %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print ("Float %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print ("integer: %d" % myint)
