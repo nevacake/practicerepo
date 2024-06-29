49a2a3c520e29166254b7e9f6b133bede87f4635
Ethan Zhang <ethanzhangyixuan@gmail.com>
2024-06-29 17:25:52 +1000

    Basic String Operations

--- python/practicepython.py
+++ python/practicepython.py
@@ -1,12 +1,46 @@
 # HELLO
 
-mystring = "hello"
-myfloat = 10.0
-myint = 20
+# mystring = "hello"
+# myfloat = 10.0
+# myint = 20
 
-if mystring == "hello":
-    print("String %s" % mystring)
-if isinstance(myfloat, float) and myfloat == 10.0:
-    print ("Float %f" % myfloat)
-if isinstance(myint, int) and myint == 20:
-    print ("integer: %d" % myint)
+# if mystring == "hello":
+#     print("String %s" % mystring)
+# if isinstance(myfloat, float) and myfloat == 10.0:
+#     print ("Float %f" % myfloat)
+# if isinstance(myint, int) and myint == 20:
+#     print ("integer: %d" % myint)
+
+# mylist = []
+# mylist.append(1)
+# mylist.append("Hello World")
+# mylist.append(12.0)
+# mylist.append({1,2,3})
+# for x in mylist:
+#     print(x)
+
+# numbers = []
+# numbers.append(1)
+# numbers.append(2)
+# numbers.append(3)
+# strings = []
+# strings.append("hello")
+# strings.append("world")
+# names = ["Eric","John","Jessica"]
+
+# second_name = names[1]
+
+# print(numbers); print(strings); print("Second name: %s" % second_name)
+
+# num1 = [2,4,6,8]
+# num2 = [1,3,5,7]
+# print(num1 + num2)
+# print([1,2,3]*3)
+
+# x = 1
+# y = 2
+
+# x_list = [x]*10
+# y_list = [y]*10
+# big_list = (x_list + y_list) *10
+# print(big_list)
